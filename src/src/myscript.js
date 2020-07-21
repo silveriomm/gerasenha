c452045d49f47420b3e74a93fde48a3a3e6b7130
Silvério Mantovaneli <silveriomm@bol.com.br>
2020-07-21 00:03:25 -0300

    first commit

---
+++ src/src/myscript.js
@@ -0,0 +1,23 @@
+const pass = document.querySelector('#pass');
+const newPassword = document.querySelector('#newPassword')
+
+function generatePass () {
+
+    newPassword.innerHTML = '';
+
+    if (pass.value === '') {
+        alert('Informe o tamanho da senha');
+        return;
+    }
+
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    let password = "";
+
+    for (var i = 0, n = charset.length; i < pass.value; ++i) {
+        password += charset.charAt(Math.floor(Math.random() * n)); 
+    }
+
+    let result = document.createTextNode(password);
+    return newPassword.appendChild(result);
+}
